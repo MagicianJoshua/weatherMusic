bb082b8d3f5e3049eb4c303215855c67c977aa7a
MagicianJoshua <joshuajordyn@hotmail.com>
2023-09-03 16:02:41 -0300

    Co-authored-by: Toua Thao <TouaThao@users.noreply.github.com>

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,33 @@
+
+// lOGIC:
+// When we hit the searchbar. It will search for our input. If the input is blank, we will recieve an error
+// If the input is not blank, it will connect with the WeatherAPI to find the city that matches the name.
+// If there are multible cities with the same name, it will ask us to specify state/province.
+// This input will be stored in a variable.
+// 
+
+
+
+
+
+
+
+
+//ATTRIBUTES
+
+
+// VARS
+
+
+
+//MAIN FUNCTION
+
+
+
+
+
+
+
 var moods = ["Happy ","Sad ","Angry ","Anxious " ,"dreamy "];
 var genres = ["metal","jazz","pop","rock","country"];
 let ran1 = Math.floor(Math.random() * genres.length);
