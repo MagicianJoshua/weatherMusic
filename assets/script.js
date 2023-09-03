b02fc9a93e242f3d2c5f754567412b24b65b3c50
MagicianJoshua <joshuajordyn@hotmail.com>
2023-09-03 14:53:57 -0300

    Got the spotify api working and i made a function that gets us the name link and image of the playlist

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-var userInfo = "happy"
+var userInfo = "sad"
 
 const url = "https://spotify23.p.rapidapi.com/search/?q="+userInfo+"&type=playlists&offset=0&limit=10&numberOfTopResults=5";
 const options = {
@@ -9,14 +9,42 @@ const options = {
 	}
 };
 
+
+
 fetch(url,options)
 .then(function(response) {
     return response.json();
 })
 .then(function(data){
-    // console.log(data.playlists.items);
+    
     let playlists = data.playlists.items;
+    let playlistArray = [];
     for (let i = 0; i < playlists.length; i++){
-        console.log(playlists[i])
+        
+        playlistArray.push(playlists[i].data);
+
     }
-})
+    PlaylistRandomizer(playlistArray);
+    
+});
+
+
+//*This function takes in the array we get from spotify and spits out a song object with a url and an image.
+function PlaylistRandomizer(array){
+    
+    let random = Math.floor(Math.random() * array.length);
+    let chosenSong = array[random];
+    //!for This array items are stored as follows, THe playlist name, the playlist uri then the playlist picture;
+    let nameUrlAndImage = [chosenSong.name,chosenSong.uri,chosenSong.images.items[0].sources[0].url]
+    console.log("name", nameUrlAndImage[0]);
+    console.log("Link", nameUrlAndImage[1]);
+    console.log("image", nameUrlAndImage[2]);
+    return nameUrlAndImage;
+}
+
+
+//*This is just a blank function for now but this it what we will use to build the spotify cards
+//*with our data.
+function cardConstructor(name,link,image){
+    
+}
