614c3f6c7491446a058d04eed300f6f642a9ecfe
Don-Patey <139824991+Don-Patey@users.noreply.github.com>
2023-09-03 17:50:18 -0300

    "fetch weather & display variables in console

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,26 @@
-// WeatherAPI inserts //
+let weather = {
+    apiKey: "cf6175175fe5277a53e5cac601d3de9d",
+     fetchWeather: function(city) {
+        fetch(
+            "https://api.openweathermap.org/data/2.5/weather?q="
+            + city +"&units=metric&appid=" 
+            + this.apiKey
+        )
+            .then((Response) => Response.json())
+            .then((data) => this.displayWeather(data));
+     },
+     displayWeather: function(data) {
+        const { name } = data;
+        const { icon, description } = data.weather[0];
+        const { temp, humidity } = data.main; 
+        console.log(name, icon, description, temp, humidity);
+     }
+     };
 
-function fetchWeather(query) {
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=cf6175175fe5277a53e5cac601d3de9d";
-fetch(queryURL)
-.then(response => response.json())
-.then(data => console.log(data));
-}
+     
 
-fetchWeather();
+    //  var moods = ["Happy ","Sad ","Angry ","Anxious " ,"dreamy "];
+    //             // sunny, rainy, thunderstorm, foggy,      
+                
+    // var weatherConditions = ["clear-sky", "few-clouds", "scattered-clouds", "broken-clouds", "shower-rain", "rain", "thunderstorm", "snow", "mist"];
+
