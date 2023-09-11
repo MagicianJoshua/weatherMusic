4f667bebe199b8f5682f4f14fc9946428b9f1b2b
MagicianJoshua <joshuajordyn@hotmail.com>
2023-09-11 20:04:41 -0300

    Changed some of the css so it looks cleaner, added a reset button that justs refreshes the page.

--- assets/script.js
+++ assets/script.js
@@ -1,22 +1,23 @@
 var playlistSearch = document.querySelector("#fullSearch");
 var searchBtn = document.querySelector(".searchbar__button");
-var genreContainer = document.querySelector(".genreContainer")
+var genreContainer = document.querySelector(".genreContainer");
 var genreHeaderEl = document.querySelector("#genreHeader");
 var searchBar = document.querySelector(".searchbar__input");
 var genreEl = document.querySelector("#genreEl");
 var weatherIcon = document.querySelector("#weatherIcon");
 var weatherHeaderEl = document.querySelector("#cityNameHeader");
 var cityNamePEl = document.querySelector("#cityNameP");
+var resetBtnEl = document.querySelector("#resetBtn");
 var weatherDescription;
 var cityImage;
 var genreUserChoice = [];
-var genres = ["metal", "jazz", "pop", "rock", "country"];
+var genres = ["metal", "jazz", "pop", "rock", "country","rap","dicso","classical","alternative"].sort();
 var placeholderImage = [
-  'assets/ImageAssets/PlaceholderBackground.PNG',
-  'assets/ImageAssets/PlaceholderBackground.PNG',
-  'assets/ImageAssets/PlaceholderBackground.PNG',
-  'assets/ImageAssets/PlaceholderBackground.PNG',
-  ];  
+  "assets/ImageAssets/PlaceholderBackground.PNG",
+  "assets/ImageAssets/PlaceholderBackground.PNG",
+  "assets/ImageAssets/PlaceholderBackground.PNG",
+  "assets/ImageAssets/PlaceholderBackground.PNG",
+];
 var userInfo = [];
 var weather = {
   name: null,
@@ -27,44 +28,30 @@ var weather = {
 };
 
 var moods = {
-  "thunderstorm":"angry",
-  "drizzle":"sad",
-  "rain":"melancholy",
+  "thunderstorm": "angry",
+  "drizzle": "sad",
+  "rain": "melancholy",
   "snow": "christmas",
-  "mist":"calm",
-  "smoke":"uneasy",
-  "haze":"tranquil",
-  "dust":"afraid",
-  "fog":"ominous",
-  "sand":"relaxed",
-  "ash":"tense",
-  "squall":"nervous",
-  "tornado":"stressed",
-  "clear":"happy",
-}
+  "mist": "calm",
+  "smoke": "uneasy",
+  "haze": "tranquil",
+  "dust": "afraid",
+  "fog": "ominous",
+  "sand": "relaxed",
+  "ash": "tense",
+  "squall": "nervous",
+  "tornado": "stressed",
+  "clear": "happy",
+  "clouds":"dreamy"
+};
 
-for (let i = 0; i < genres.length; i++) {
-  let btn = document.createElement("button");
-  btn.textContent = genres[i];
-  btn.setAttribute("id", genres[i]);
-  btn.setAttribute("class", "genreBtn");
-  btn.addEventListener("click", function (event) {
-    event.preventDefault();
-    let genre = this.getAttribute("id");
-    genreUserChoice.push(genre);
-
-    if (genreUserChoice.length > 1) {
-      let removeChoice = genreUserChoice.shift();
-    }
-    userInfo[1] = genreUserChoice[0];
-    genreHeaderEl.textContent = "Current selected genre: " + genreUserChoice[0];
-  });
-  genreEl.appendChild(btn);
-}
+resetBtnEl.addEventListener("click", function(event) {
+  console.log("Refreshing page");
+})
 
 searchBtn.addEventListener("click", function (event) {
   event.preventDefault();
-  console.log(searchBar.value)
+  console.log(searchBar.value);
   weatherApi(searchBar.value).then(function (data) {
     weather.icon = data.weather[0].icon;
     console.log(weather.icon);
@@ -72,85 +59,113 @@ searchBtn.addEventListener("click", function (event) {
     weather.temp = data.main.temp;
     weather.description = data.weather[0].description;
     weather.main = data.weather[0].main.toLowerCase();
+    console.log(weather.main);
     let icon = weather.icon;
     let iconUrl = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
     weatherIcon.setAttribute("src", iconUrl);
     weatherHeaderEl.textContent = weather.name;
-    pullCityPhoto(searchBar.value).then(function (data){
+    pullCityPhoto(searchBar.value).then(function (data) {
       cityImage = data.photos[0];
       renderCityPhoto(cityImage);
-    })
+    });
   });
+
+  genreHeaderEl.style.display = "flex";
+  playlistSearch.style.display = "flex";
+
+  for (let i = 0; i < genres.length; i++) {
+    let btn = document.createElement("button");
+    btn.textContent = genres[i];
+    btn.setAttribute("id", genres[i]);
+    btn.setAttribute("class", "genreBtn");
+    btn.addEventListener("click", function (event) {
+      event.preventDefault();
+      let genre = this.getAttribute("id");
+      genreUserChoice.push(genre);
+  
+      if (genreUserChoice.length > 1) {
+        let removeChoice = genreUserChoice.shift();
+      }
+      userInfo[1] = genreUserChoice[0];
+      genreHeaderEl.textContent = "Current selected genre: " + genreUserChoice[0];
+    });
+    genreEl.appendChild(btn);
+  }
 });
 
 playlistSearch.addEventListener("click", function (event) {
   event.preventDefault();
   let searchChoice = searchPlaylist();
   const url =
-  "https://spotify23.p.rapidapi.com/search/?q=" +
-  searchChoice +
-  "&type=playlists&offset=0&limit=50&numberOfTopResults=5";
+    "https://spotify23.p.rapidapi.com/search/?q=" +
+    searchChoice +
+    "&type=playlists&offset=0&limit=50&numberOfTopResults=5";
   const options = {
-        method: "GET",
-        headers: {
-        "X-RapidAPI-Key": "6eb0f56eacmsh4879f7ba423d0f6p1f0f15jsn0aec5d290516",
-        "X-RapidAPI-Host": "spotify23.p.rapidapi.com",
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "6eb0f56eacmsh4879f7ba423d0f6p1f0f15jsn0aec5d290516",
+      "X-RapidAPI-Host": "spotify23.p.rapidapi.com",
     },
   };
 
-    fetch(url, options)
-        .then(function (response) {
-          return response.json();
-        })
-        .then(function (data) {
-          let playlists = data.playlists.items;
-          let playlistArray = [];
-          for (let i = 0; i < playlists.length; i++) {
-            playlistArray.push(playlists[i].data);
-          }
-          let chosenList = PlaylistRandomizer(playlistArray);
-          cardConstructor(chosenList[0], chosenList[1], chosenList[2]);
-        });
+  fetch(url, options)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      let playlists = data.playlists.items;
+      let playlistArray = [];
+      for (let i = 0; i < playlists.length; i++) {
+        playlistArray.push(playlists[i].data);
+      }
+      let chosenList = PlaylistRandomizer(playlistArray);
+      cardConstructor(chosenList[0], chosenList[1], chosenList[2]);
+    });
+
+    resetBtnEl.style.display = "flex";
 });
 
+function searchPlaylist() {
+  userInfo[0] = moods[weather.main];
+  let userChoice = userInfo.join(" ");
+  genreContainer.style.display = "none";
+  return userChoice;
+}
 
 // Aston: Above array will hold a few seperate image assets that will be the defaults for when the API fails to find
 // a city (free API so it only has major cities). May find an alternative if I have enough time as the images returned
 // from the API are quite low resolution.
 
-function pullCityPhoto(name){
-  var cityURL = 'https://api.teleport.org/api/urban_areas/slug:' + name + '/images/'
+function pullCityPhoto(name) {
+  var cityURL =
+    "https://api.teleport.org/api/urban_areas/slug:" + name + "/images/";
   var cityName = document.getElementById("cityName");
   cityName.textContent = name;
   return fetch(cityURL)
-    .then(function (response){
+    .then(function (response) {
       return response.json();
-  }).then(function (data) {
-    return data
-  })
-  .catch(function (error) {
-    console.log(error);
-  })
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
 }
 //Aston: Above function will pullCityPhoto, using the VAR 'name'. I don't think that is correct. We need to pull user
 // input but I don't know where that gets pulled. Note that line 97 pulls Moncton as a static.
 
-
 //Aston: Above is to recieve userInput and set the background image using the renderCityPhoto function.
 //Currently defaulted to Quebec, needs proper user input.
 
-function renderCityPhoto(image){
+function renderCityPhoto(image) {
   var backGroundImage = document.getElementById("image-background");
   backGroundImage.src = image.image.web;
 }
 //ASton: This can probably be combined with pullCityPhoto?
 
-function randomPhoto(){
-  console.log('error: NO IMAGE FOUND IN pullCityPhoto function')
+function randomPhoto() {
+  console.log("error: NO IMAGE FOUND IN pullCityPhoto function");
   backGroundImage.src = "assets/ImageAssets/PlaceholderBackground.PNG";
-//Aston: This function will pull a random local image when the Photo API fails.
+  //Aston: This function will pull a random local image when the Photo API fails.
 }
-
 
 //this is the weatherApi function
 function weatherApi(city) {
@@ -160,13 +175,10 @@ function weatherApi(city) {
     city +
     "&units=metric&appid=" +
     apiKey;
-    return fetch(url)
-      .then((Response) => Response.json())
-      .then((data) => data);
-};
-
-
-
+  return fetch(url)
+    .then((Response) => Response.json())
+    .then((data) => data);
+}
 
 //*This function takes in the array we get from spotify and spits out a song object with a url and an image.
 function PlaylistRandomizer(array) {
@@ -207,15 +219,5 @@ function cardConstructor(name, link, image) {
 
   playlistContainer.appendChild(Container);
 
-  playlistContainer.style.display = "flex"
+  playlistContainer.style.display = "flex";
 }
-
-
-function searchPlaylist() {
-  userInfo[0] = "happy";
-  let userChoice = userInfo.join(" ");
-  genreContainer.style.display = "none";
-  return userChoice;
-
-}
-
